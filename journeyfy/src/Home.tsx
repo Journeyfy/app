e3645cc3a74070e140cdb4958e2f955eef6d08d5
Lukedv98 <lukev98@gmail.com>
2024-01-03 19:54:07 +0100

    Popolamento dinamico nella select e click sulla destinazione

--- journeyfy/src/Home.tsx
+++ journeyfy/src/Home.tsx
@@ -1,17 +1,37 @@
 import React, { Fragment, useState } from 'react';
 import './index.css';
 import BarraRicerca from './BarraRicerca';
-
+import DynamicSelect from './DynamicSelect';
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  // Definisci le opzioni direttamente all'interno del componente Home
+  const optionsData = ["Opzione 1", "Opzione 2", "Opzione 3"];
+  const navigate = useNavigate(); // Cambiato da useHistory a useNavigate
+  const [selectedOption, setSelectedOption] = useState<string>('');
+
   function handleSearch(searchTerm: string): void {
-    throw new Error('Function not implemented.');
+    //throw new Error('Function not implemented.');
   }
+
+  const handleSelectChange = (selectedValue: string): void => {
+    setSelectedOption(selectedValue);
+
+    // Esegui il reindirizzamento alla root destination associata all'opzione selezionata
+    if (selectedValue === 'Opzione 1') {
+      navigate('/activity1');
+    } else if (selectedValue === 'Opzione 2') {
+      navigate('/activity2');
+    } // Posso aggiungere altri casi a seconda delle opzioni
+
+    // Posso anche implementare una logica più dinamica basata sulle opzioni e sulle destinazioni
+  };
 
   return (<Fragment><h2>Esplora</h2>
     <div>
       <h1>Applicazione con Barra di Ricerca</h1>
       <BarraRicerca onSearch={handleSearch} />
+      <DynamicSelect options={optionsData} onSelectChange={handleSelectChange} />
       {/* Altri contenuti dell'applicazione possono essere aggiunti qui */}
     </div></Fragment>)
 
