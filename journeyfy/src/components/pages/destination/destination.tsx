315a3851cd9c3be684991be743dde92a86215791
Lukedv98 <lukev98@gmail.com>
2024-02-07 19:50:08 +0100

    Aggiunta del side menù

--- journeyfy/src/components/pages/destination/destination.tsx
+++ journeyfy/src/components/pages/destination/destination.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Button from 'dolfo/comps/layout/Button';
+import { MenuItem, SideMenu } from 'dolfo';
 import './destination.css';
-
 
 interface DestinationProps {
   cityName: string;
@@ -9,24 +10,74 @@ interface DestinationProps {
 }
 
 const Destination: React.FC<DestinationProps> = ({ cityName, coverImage }) => {
+  const [currentMenu, setCurrentMenu] = useState<string | null>(null);
+
+  const openMenu = (menuName: string) => {
+    setCurrentMenu(menuName);
+  };
+
+  const closeMenu = () => {
+    setCurrentMenu(null);
+  };
+
   return (
     <div className="destination-container">
-      <button className="return-button">
-        <Link to="/">Indietro</Link>
-      </button>
-      <h1>{cityName}</h1>
-      <img className="destination-cover-image" src={coverImage} alt={`Copertina di ${cityName}`} />
-      <div className="button-container">
-        <button>Luoghi da vedere</button>
-        <button>Attività</button>
-        <button>Locali</button>
+      <div className="content-container">
+        <h1>{cityName}</h1>
+        <img className="destination-cover-image" src={coverImage} alt={`Copertina di ${cityName}`} />
+        <div className="button-container">
+          <button onClick={() => openMenu('Luoghi da vedere')}>Luoghi da vedere</button>
+          <button onClick={() => openMenu('Attività')}>Attività</button>
+          <button onClick={() => openMenu('Locali')}>Locali</button>
+        </div>
       </div>
+      
+      <SideMenu style={{ background: '#999999' }} direction="right" opened={currentMenu !== null} onToggle={closeMenu}>
+        <div style={{ textAlign: 'center', padding: '20px' }}>
+          <h2>{currentMenu}</h2>
+          {currentMenu === 'Luoghi da vedere' && (
+            <>
+              <MenuItem>
+              <Link to="/">Luogo 1</Link>
+              </MenuItem>
+              <MenuItem>
+              <Link to="/">Luogo 2</Link>
+              </MenuItem>
+              <MenuItem>
+              <Link to="/">Luogo 3</Link>
+              </MenuItem>
+            </>
+          )}
+          {currentMenu === 'Attività' && (
+            <>
+              <MenuItem>
+              <Link to="/">Attività 1</Link>
+              </MenuItem>
+              <MenuItem>
+              <Link to="/">Attività 2</Link>
+              </MenuItem>
+              <MenuItem>
+              <Link to="/">Attività 3</Link>
+              </MenuItem>
+            </>
+          )}
+          {currentMenu === 'Locali' && (
+            <>
+              <MenuItem>
+              <Link to="/">Locale 1</Link>
+              </MenuItem>
+              <MenuItem>
+              <Link to="/">Locale 2</Link>
+              </MenuItem>
+              <MenuItem>
+              <Link to="/">Locale 3</Link>
+              </MenuItem>
+            </>
+          )}
+        </div>
+      </SideMenu>
     </div>
   );
 };
 
 export default Destination;
-
-/*export function Destination(){
-    return <div>Ciao da Destination</div>
-}*/
