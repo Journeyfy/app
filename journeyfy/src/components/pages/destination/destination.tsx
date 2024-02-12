7e42a5ae1664fdc48bc2dfb9208ef6d37a02e84c
Alessandro Di Roma <diromaalessandro@gmail.com>
2024-02-12 10:05:32 +0100

    Review suggestion dinamiche

--- journeyfy/src/components/pages/destination/destination.tsx
+++ journeyfy/src/components/pages/destination/destination.tsx
@@ -1,28 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { MenuItem, SideMenu } from 'dolfo';
-import { Destination as DestinationType } from '../../../models/destination';
-import { DestinationApiService } from '../../../services/destinationApiService';
-import { SuggestionType } from '../../../enums/suggestionType';
-import { Suggestion } from '../../../models/suggestion';
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { MenuItem, SideMenu } from "dolfo";
+import { Destination as DestinationType } from "../../../models/destination";
+import { DestinationApiService } from "../../../services/destinationApiService";
+import { SuggestionType } from "../../../enums/suggestionType";
+import { Suggestion } from "../../../models/suggestion";
 
 const Destination: React.FC = () => {
-  const [destinationInfo, setDestinationInfo] = useState<DestinationType | null>(null);
+  const [destinationInfo, setDestinationInfo] =
+    useState<DestinationType | null>(null);
   const [suggestionCategories, setSuggestionCategories] = useState<
     { type: SuggestionType; suggestions: Suggestion[] }[]
   >([]);
   const { destinationId } = useParams();
-  const [currentMenu, setCurrentMenu] = useState<string | null>(null);
+  const [currentMenu, setCurrentMenu] = useState<SuggestionType | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [info, toSeeSuggestions, toDoSuggestions, toEatSuggestions] = await Promise.all([
-          DestinationApiService.getDestinationInfo(Number(destinationId)),
-          DestinationApiService.getDestinationSuggestions(Number(destinationId), SuggestionType.ToSee),
-          DestinationApiService.getDestinationSuggestions(Number(destinationId), SuggestionType.ToDo),
-          DestinationApiService.getDestinationSuggestions(Number(destinationId), SuggestionType.ToEat),
-        ]);
+        const [info, toSeeSuggestions, toDoSuggestions, toEatSuggestions] =
+          await Promise.all([
+            DestinationApiService.getDestinationInfo(Number(destinationId)),
+            DestinationApiService.getDestinationSuggestions(
+              Number(destinationId),
+              SuggestionType.ToSee
+            ),
+            DestinationApiService.getDestinationSuggestions(
+              Number(destinationId),
+              SuggestionType.ToDo
+            ),
+            DestinationApiService.getDestinationSuggestions(
+              Number(destinationId),
+              SuggestionType.ToEat
+            ),
+          ]);
 
         setDestinationInfo(info);
         setSuggestionCategories([
@@ -31,20 +42,35 @@ const Destination: React.FC = () => {
           { type: SuggestionType.ToEat, suggestions: toEatSuggestions },
         ]);
       } catch (error) {
-        console.error('Errore durante il recupero dei dati:', error);
+        console.error("Errore durante il recupero dei dati:", error);
       }
     };
 
     fetchData();
   }, [destinationId]);
 
-  const openMenu = (menuName: string) => {
+  const openMenu = (menuName: SuggestionType) => {
     setCurrentMenu(menuName);
   };
 
   const closeMenu = () => {
     setCurrentMenu(null);
   };
+
+  const mapSuggestionTypeToString = (status: SuggestionType) => {
+    switch (status) {
+      case SuggestionType.ToSee:
+        return "Cosa vedere";
+      case SuggestionType.ToDo:
+        return "Cosa fare";
+      case SuggestionType.ToEat:
+        return "Dove mangiare";
+    }
+  };
+
+  const suggestionTypeValues = Object.values(SuggestionType).filter(
+    (v) => !isNaN(Number(v))
+  );
 
   return (
     <>
@@ -59,8 +85,11 @@ const Destination: React.FC = () => {
             />
             <div className="button-container">
               {suggestionCategories.map((category) => (
-                <button key={category.type} onClick={() => openMenu(category.type.toString())}>
-                  {category.type}
+                <button
+                  key={category.type}
+                  onClick={() => openMenu(category.type)}
+                >
+                  {mapSuggestionTypeToString(category.type)}
                 </button>
               ))}
             </div>
@@ -68,17 +97,30 @@ const Destination: React.FC = () => {
             {suggestionCategories.map((category) => (
               <SideMenu
                 key={category.type}
-                style={{ background: '#999999' }}
+                style={{ background: "#999999" }}
                 direction="right"
-                opened={currentMenu === category.type.toString()}
+                opened={currentMenu === category.type}
                 onToggle={closeMenu}
               >
-                <div style={{ textAlign: 'center', padding: '20px' }}>
-                  <h2>{category.type}</h2>
+                <div style={{ padding: "20px" }}>
+                  <h2>{mapSuggestionTypeToString(category.type)}</h2>
                   {category.suggestions.map((suggestion) => (
-                    <MenuItem key={suggestion.id}>
-                     <a href={suggestion.mapLink}>{suggestion.title}</a>
-                    </MenuItem>
+                    <div className="d-flex align-items-center">
+                      <div>
+                        <a href={suggestion.mapLink} target="__blank">
+                          {suggestion.title}
+                        </a>
+                        {suggestion.openAt && (
+                          <span className="mx-2">{suggestion.openAt}</span>
+                        )}
+                        {suggestion.closeAt && (
+                          <>
+                            -<span className="mx-2">{suggestion.closeAt}</span>
+                          </>
+                        )}
+                      </div>
+                      {/* <div>TODO: bottoni</div> */}
+                    </div>
                   ))}
                 </div>
               </SideMenu>
